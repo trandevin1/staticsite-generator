76c9b18d94d092da351abe6cea6cae9ebd9cfa0a
trandevin1 <trandevin2@gmail.com>
2024-09-13 11:30:02 -0700

    added copy function and generate_page function

--- src/main.py
+++ src/main.py
@@ -1,10 +1,6 @@
-from textnode import TextNode
+from block_markdown import markdown_to_html_node, extract_title
 import os
 import shutil
-
-
-def main():
-    node1 = TextNode("Some text", "type1", "www.testurl.com")
 
 
 def copy(source, destination):
@@ -34,8 +30,56 @@ def copy(source, destination):
     return 0
 
 
+def generate_page(from_path, template_path, dest_path):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}.")
+
+    markdown = ""
+    template = ""
+
+    with open(from_path, "r") as file:
+        markdown = file.read()
+
+    with open(template_path, "r") as file:
+        template = file.read()
+
+    """ print(markdown)
+    print("\n\n\n")
+    print(template)
+ """
+    res = markdown_to_html_node(markdown).to_html()
+
+    # print(res)
+
+    title = extract_title(markdown)
+    # print(title)
+
+    template = template.replace("{{ Title }}", title)
+    template = template.replace("{{ Content }}", res)
+    # print(template)
+
+    # grab the directories
+    destination = os.path.dirname(dest_path)
+
+    # check to see if the destination directory already exists
+    if not os.path.exists(destination):
+        os.makedirs(destination)
+
+    with open(dest_path, "w") as file:
+        file.write(template)
+
+    return None
+
+
+def main():
+    static_path = "/home/tran/Projects/boot_dev/staticsite/static"
+    public_path = "/home/tran/Projects/boot_dev/staticsite/public"
+    source_path = "/home/tran/Projects/boot_dev/staticsite/content/index.md"
+    template_path = "/home/tran/Projects/boot_dev/staticsite/template.html"
+    dest_path = "/home/tran/Projects/boot_dev/staticsite/public/index.html"
+
+    copy(static_path, public_path)
+    generate_page(source_path, template_path, dest_path)
+
+
 if __name__ == "__main__":
-    copy(
-        "/home/tran/Projects/boot_dev/staticsite/static",
-        "/home/tran/Projects/boot_dev/staticsite/public",
-    )
+    main()
